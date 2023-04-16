d9d651dcc6b566ea08d94ce8fd0f93b3a185d5ca
Pedro Germano <pedrogermano232@gmail.com>
2023-04-15 22:47:10 -0300

    aside buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
+import { Home as HomeIcon, Search, Library} from 'lucide-react';
+
+
 export default function Home(){
   return(
     <section className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-64 bg-zinc-950 p-6">
-          sidebar
+        <aside className="w-72 bg-zinc-950 p-6">
+          <div className="flex items-center gap-2">
+            <div className='w-3 h-3 bg-red-500 rounded-full' />
+            <div className='w-3 h-3 bg-yellow-500 rounded-full' />
+            <div className='w-3 h-3 bg-green-500 rounded-full' />
+          </div>
+          
+          <nav className='space-y-5 mt-10'>
+            <a href="#" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <HomeIcon />
+              Home
+            </a>
+            <a href="#" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Search />
+              Search
+            </a>
+            <a href="#" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Library />
+              You Library
+            </a>
+          </nav>
         </aside>
         <main className="flex-1 p-6">
           main
