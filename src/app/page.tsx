29476a83113855bcea4509231e96e99217a0f21b
Pedro Germano <pedrogermano232@gmail.com>
2023-04-16 14:00:26 -0300

    New section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,86 +1,182 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play} from 'lucide-react';
-import Image from 'next/image';
+import {
+  Home as HomeIcon,
+  Search,
+  Library,
+  ChevronLeft,
+  ChevronRight,
+  Play,
+} from "lucide-react";
+import Image from "next/image";
 
-
-export default function Home(){
-  return(
+export default function Home() {
+  return (
     <section className="h-screen flex flex-col">
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
           <div className="flex items-center gap-2">
-            <div className='w-3 h-3 bg-red-500 rounded-full' />
-            <div className='w-3 h-3 bg-yellow-500 rounded-full' />
-            <div className='w-3 h-3 bg-green-500 rounded-full' />
+            <div className="w-3 h-3 bg-red-500 rounded-full" />
+            <div className="w-3 h-3 bg-yellow-500 rounded-full" />
+            <div className="w-3 h-3 bg-green-500 rounded-full" />
           </div>
-          
-          <nav className='space-y-5 mt-10'>
-            <a href="#" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+
+          <nav className="space-y-5 mt-10">
+            <a
+              href="#"
+              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
+            >
               <HomeIcon />
               Home
             </a>
-            <a href="#" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+            <a
+              href="#"
+              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
+            >
               <Search />
               Search
             </a>
-            <a href="#" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+            <a
+              href="#"
+              className="flex items-center gap-3 text-sm font-semibold text-zinc-200"
+            >
               <Library />
               You Library
             </a>
           </nav>
 
-          <nav className='mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3'>
-            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="#">Rock Nacional</a>
-            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="#">Heavy Metal</a>
-            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="#">Hard Core</a>
-            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="#">Power Metal</a>
+          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="#">
+              Rock Nacional
+            </a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="#">
+              Heavy Metal
+            </a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="#">
+              Hard Core
+            </a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100" href="#">
+              Power Metal
+            </a>
           </nav>
         </aside>
         <main className="flex-1 p-6">
-          <div className='flex items-center gap-4'>
-            <button className='rounded-full bg-black/40 p-1'>
-              <ChevronLeft  />
+          <div className="flex items-center gap-4">
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronLeft />
             </button>
-            <button className='rounded-full bg-black/30 p-1'>
-              <ChevronRight  />
+            <button className="rounded-full bg-black/30 p-1">
+              <ChevronRight />
             </button>
           </div>
 
-          <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
+          <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
 
-          <div className='grid grid-cols-3 gap-4 mt-4'>
-            <a href="#" className='bg-white/10 rounded flex items-center gap-4 overflow-hidden'>
-              <Image src="/maiden.jpg" width={104} height={104} alt='capa'/>
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a
+              href="#"
+              className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition"
+            >
+              <Image src="/maiden.jpg" width={104} height={104} alt="capa" />
               <strong>Killers</strong>
-              <button className='w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-400 text-black ml-auto mr-8'>
-                  <Play />
+              <button
+                className="w-12 h-12 flex items-center justify-center pl-0.5
+                rounded-full bg-green-400 text-black ml-auto mr-8 invisible 
+                group-hover:visible"
+              >
+                <Play />
               </button>
             </a>
-            <a href="#" className='bg-white/10 rounded flex items-center gap-4 overflow-hidden'>
-              <Image src="/maiden.jpg" width={104} height={104} alt='capa'/>
+            <a
+              href="#"
+              className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition"
+            >
+              <Image src="/maiden.jpg" width={104} height={104} alt="capa" />
               <strong>Killers</strong>
+              <button
+                className="w-12 h-12 flex items-center justify-center pl-0.5
+                rounded-full bg-green-400 text-black ml-auto mr-8 invisible 
+                group-hover:visible"
+              >
+                <Play />
+              </button>
             </a>
-            <a href="#" className='bg-white/10 rounded flex items-center gap-4 overflow-hidden'>
-              <Image src="/maiden.jpg" width={104} height={104} alt='capa'/>
+            <a
+              href="#"
+              className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition"
+            >
+              <Image src="/maiden.jpg" width={104} height={104} alt="capa" />
               <strong>Killers</strong>
+              <button
+                className="w-12 h-12 flex items-center justify-center pl-0.5
+                rounded-full bg-green-400 text-black ml-auto mr-8 invisible 
+                group-hover:visible"
+              >
+                <Play />
+              </button>
             </a>
-            <a href="#" className='bg-white/10 rounded flex items-center gap-4 overflow-hidden'>
-              <Image src="/maiden.jpg" width={104} height={104} alt='capa'/>
+            <a
+              href="#"
+              className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition"
+            >
+              <Image src="/maiden.jpg" width={104} height={104} alt="capa" />
               <strong>Killers</strong>
+              <button
+                className="w-12 h-12 flex items-center justify-center pl-0.5
+                rounded-full bg-green-400 text-black ml-auto mr-8 invisible 
+                group-hover:visible"
+              >
+                <Play />
+              </button>
             </a>
-            <a href="#" className='bg-white/10 rounded flex items-center gap-4 overflow-hidden'>
-              <Image src="/maiden.jpg" width={104} height={104} alt='capa'/>
+            <a
+              href="#"
+              className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition"
+            >
+              <Image src="/maiden.jpg" width={104} height={104} alt="capa" />
               <strong>Killers</strong>
+              <button
+                className="w-12 h-12 flex items-center justify-center pl-0.5
+                rounded-full bg-green-400 text-black ml-auto mr-8 invisible 
+                group-hover:visible"
+              >
+                <Play />
+              </button>
             </a>
-            <a href="#" className='bg-white/10 rounded flex items-center gap-4 overflow-hidden'>
-              <Image src="/maiden.jpg" width={104} height={104} alt='capa'/>
+            <a
+              href="#"
+              className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden"
+            >
+              <Image src="/maiden.jpg" width={104} height={104} alt="capa" />
               <strong>Killers</strong>
+              <button
+                className="w-12 h-12 flex items-center justify-center pl-0.5
+                rounded-full bg-green-400 text-black ml-auto mr-8 invisible 
+                group-hover:visible"
+              >
+                <Play />
+              </button>
             </a>
+          </div>
+
+          <h2 className="font-semibold text-2xl mt-10">
+            Made for Pedro Germano
+          </h2>
+
+          <div className="grid grid-cols-5 gap-4 mt-4">
+            <div className="bg-white/5 p-2 rounded">
+              <Image
+                className="w-full"
+                src="/maiden.jpg"
+                width={120}
+                height={120}
+                alt="capa"
+              />
+            </div>
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+      <footer className="bg-zinc-800 border-t border-zinc-700 hover:bg-white/20 transition p-6">
         footer
       </footer>
     </section>
-  )
+  );
 }
