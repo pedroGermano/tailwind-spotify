9afc0cdb7034b2cc9970f755b4127059169c6acf
Pedro Germano <pedrogermano232@gmail.com>
2023-04-16 17:13:50 -0300

    Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,16 @@ import {
   ChevronLeft,
   ChevronRight,
   Play,
+  Shuffle,
+  SkipBack,
+  SkipForward,
+  Repeat,
+  Mic2,
+  LayoutList,
+  Laptop2,
+  Volume,
+  Maximize,
+  Maximize2,
 } from "lucide-react";
 import Image from "next/image";
 
@@ -161,8 +171,11 @@ export default function Home() {
             Made for Pedro Germano
           </h2>
 
-          <div className="grid grid-cols-5 gap-4 mt-4">
-            <div className="bg-white/5 p-2 rounded">
+          <div className="grid grid-cols-8 gap-4 mt-4">
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded flex flex-col gap-2 hover:bg-white/10"
+            >
               <Image
                 className="w-full"
                 src="/maiden.jpg"
@@ -170,12 +183,107 @@ export default function Home() {
                 height={120}
                 alt="capa"
               />
-            </div>
+              <strong className="font-semibold">Daily Mix 1 </strong>
+              <span className="text-sm text-zinc-500">Iron Maiden Killers</span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded flex flex-col gap-2 hover:bg-white/10"
+            >
+              <Image
+                className="w-full"
+                src="/maiden.jpg"
+                width={120}
+                height={120}
+                alt="capa"
+              />
+              <strong className="font-semibold">Daily Mix 1 </strong>
+              <span className="text-sm text-zinc-500">Iron Maiden Killers</span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded flex flex-col gap-2 hover:bg-white/10"
+            >
+              <Image
+                className="w-full"
+                src="/maiden.jpg"
+                width={120}
+                height={120}
+                alt="capa"
+              />
+              <strong className="font-semibold">Daily Mix 1 </strong>
+              <span className="text-sm text-zinc-500">Iron Maiden Killers</span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded flex flex-col gap-2 hover:bg-white/10"
+            >
+              <Image
+                className="w-full"
+                src="/maiden.jpg"
+                width={120}
+                height={120}
+                alt="capa"
+              />
+              <strong className="font-semibold">Daily Mix 1 </strong>
+              <span className="text-sm text-zinc-500">Iron Maiden Killers</span>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 p-3 rounded flex flex-col gap-2 hover:bg-white/10"
+            >
+              <Image
+                className="w-full"
+                src="/maiden.jpg"
+                width={120}
+                height={120}
+                alt="capa"
+              />
+              <strong className="font-semibold">Daily Mix 1 </strong>
+              <span className="text-xs text-zinc-500">Iron Maiden Killers</span>
+            </a>
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 hover:bg-white/20 transition p-6">
-        footer
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <Image src="/maiden.jpg" width={56} height={56} alt="capa" />
+          <div className="flex flex-col">
+            <strong className="font-normal">Running Free</strong>
+            <span className="text-xs text-zinc-400">Iron Maiden</span>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex items-center gap-6">
+            <Shuffle size={20} className="text-zinc-200" />
+            <SkipBack size={20} className="text-zinc-200" />
+            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
+              <Play />
+            </button>
+
+            <SkipForward size={20} className="text-zinc-200" />
+            <Repeat size={20} className="text-zinc-200" />
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc- 400">0:31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
+            </div>
+            <span className="text-xs text-zinc-400">2:14</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <Mic2 size={20} />
+          <LayoutList size={20} />
+          <Laptop2 size={20} />
+          <div className="flex items-center gap-2">
+            <Volume size={20} />  
+            <div className="h-1 rounded-full w-24 bg-zinc-600">
+              <div className="bg-zinc-200 w-10 h-1 rounded-full"></div>
+            </div>
+          </div>
+          <Maximize2 size={20} />
+        </div>
       </footer>
     </section>
   );
